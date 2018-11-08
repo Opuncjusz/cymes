438db9fd394ef4c896d5a11e1def90a9d46856e6
bogna <bogna.wrobel@capgemini.com>
2018-11-08 14:27:18 +0100

    create repos and entities

--- src/main/java/pl/com/bottega/cymes/admin/Cinema.java
+++ src/main/java/pl/com/bottega/cymes/admin/Cinema.java
@@ -8,7 +8,11 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Index;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -26,4 +30,6 @@ public class Cinema {
     private String name;
     private String city;
 
+    @OneToMany
+    Set<CinemaHall> cinemaHalls;
 }
