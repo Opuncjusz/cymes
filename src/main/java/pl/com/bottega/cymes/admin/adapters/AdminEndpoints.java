1ad0461efc87d35f676cf84273c78250c289b5ea
Aron Mojsak <external.aron.mojsak@kuehne-nagel.com>
2018-11-09 10:10:54 +0100

    fix compile

--- src/main/java/pl/com/bottega/cymes/admin/adapters/AdminEndpoints.java
+++ src/main/java/pl/com/bottega/cymes/admin/adapters/AdminEndpoints.java
@@ -1,11 +1,17 @@
 package pl.com.bottega.cymes.admin.adapters;
 
-import lombok.AllArgsConstructor;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.transaction.Transactional;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
 import pl.com.bottega.cymes.admin.Cinema;
 import pl.com.bottega.cymes.admin.CinemaHall;
 import pl.com.bottega.cymes.admin.CinemaHallRepository;
@@ -17,12 +23,7 @@ import pl.com.bottega.cymes.admin.CreateShow;
 import pl.com.bottega.cymes.admin.Movie;
 import pl.com.bottega.cymes.admin.MovieRepository;
 import pl.com.bottega.cymes.admin.Show;
-import pl.com.bottega.cymes.admin.ShowRepository;
 import pl.com.bottega.cymes.admin.UpdateCinema;
-
-import javax.transaction.Transactional;
-import java.util.HashSet;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/admin")
@@ -32,16 +33,15 @@ public class AdminEndpoints {
     private CinemaRepository cinemaRepository;
     private MovieRepository movieRepository;
     private CinemaHallRepository cinemaHallRepository;
-    private ShowRepository showRepository;
 
     @PostMapping("/cinemas")
     @Transactional
     public void createCinema(@RequestBody CreateCinema createCinema) {
         Cinema cinema = Cinema.builder()
-            .city(createCinema.city)
-            .id(createCinema.cinemaId)
-            .name(createCinema.name)
-            .build();
+                .city(createCinema.city)
+                .id(createCinema.cinemaId)
+                .name(createCinema.name)
+                .build();
         cinema = cinemaRepository.save(cinema);
     }
 
@@ -57,12 +57,12 @@ public class AdminEndpoints {
     @Transactional
     public void createMovie(@RequestBody CreateMovie createMovie) {
         Movie movie = Movie.builder()
-            .movieId(createMovie.movieId)
-            .title(createMovie.title)
-            .description(createMovie.description)
-            .actors(createMovie.actors)
-            .genres(createMovie.genres)
-            .build();
+                .movieId(createMovie.movieId)
+                .title(createMovie.title)
+                .description(createMovie.description)
+                .actors(createMovie.actors)
+                .genres(createMovie.genres)
+                .build();
         movieRepository.save(movie);
     }
 
@@ -71,23 +71,23 @@ public class AdminEndpoints {
     public void createHall(@RequestBody CreateCinemaHall cmd) {
         Cinema cinema = cinemaRepository.findById(cmd.cinemaId);
         CinemaHall cinemaHall = CinemaHall.builder()
-            .cinema(cinema)
-            .id(cmd.cinemaId)
-            .name(cmd.hallName)
-            .hallElements(mapHallElements(cmd.seats))
-            .build();
+                .cinema(cinema)
+                .id(cmd.cinemaId)
+                .name(cmd.hallName)
+                .hallElements(mapHallElements(cmd.seats))
+                .build();
         cinemaHallRepository.save(cinemaHall);
     }
 
     @PostMapping("/cinemas/shows")
     public void createShow(@RequestBody CreateShow createShow) {
         Show show = Show.builder()
-            .cinemaHall(cinemaHallRepository.getOne(createShow.hallId))
-            .id(createShow.showId)
-            .movie(movieRepository.getOne(createShow.movieId))
-            .pricing(createShow.pricing)
-            .time(createShow.time)
-            .build();
+                .cinemaHall(cinemaHallRepository.getOne(createShow.hallId))
+                .id(createShow.showId)
+                .movie(movieRepository.getOne(createShow.movieId))
+                .pricing(createShow.pricing)
+                .time(createShow.time)
+                .build();
     }
 
     private Set<CinemaHall.HallElement> mapHallElements(String[][] seats) {
